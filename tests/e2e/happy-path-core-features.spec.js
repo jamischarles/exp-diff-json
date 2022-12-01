f628cd03d174987e3dbfb9cb835a6126fa42d278
Jamis Charles <jacharles@paypal.com>
2022-11-30 19:28:27 -0700

    feat(ignore-key-order): If key order is different it'll still show a match

--- tests/e2e/happy-path-core-features.spec.js
+++ tests/e2e/happy-path-core-features.spec.js
@@ -1,8 +1,11 @@
 // @ts-check
 import { test, expect } from "@playwright/test";
 
+
+// FIXME: How can I group these better by feature? Different files?
+
 // main UI
-test("will diff 2 JSON strings with green for match", async ({
+test("shows 2 input fields. If JSON string in fields MATCHES indicator shows GREEN", async ({
   page,
   baseURL,
 }) => {
@@ -27,7 +30,7 @@ test("will diff 2 JSON strings with green for match", async ({
 
 });
 
-test("will diff 2 JSON strings with red for MISMATCH", async ({
+test("shows 2 input fields. If JSON string in fields DOES NOT MATCH indicator shows RED", async ({
   page,
   baseURL,
 }) => {
@@ -49,5 +52,31 @@ test("will diff 2 JSON strings with red for MISMATCH", async ({
   await secondDiffInput.fill(valB);
 
   await expect(diffStatusIndicator).toHaveClass(/diff-status-not-same/);
+
+});
+
+// JSON DIFF FEATURES
+test("ignore JSON KEY order: 2 input fields indicate MATCH if JSON keys are in wrong order, but are otherwise identical", async ({
+  page,
+  baseURL,
+}) => {
+  await page.goto(baseURL + "/");
+
+  // am I on the right page?
+  await expect(page).toHaveTitle(/Differ/);
+
+  const valA = `{"a": "testA", "b": "testB"}`;
+  const valB = `{"b": "testB", "a": "testA"}`;
+
+  // locators
+  const firstDiffInput = page.getByTestId("diff-field-a");
+  const secondDiffInput = page.getByTestId("diff-field-b");
+  const diffStatusIndicator = page.getByTestId("diff-status-indicator");
+
+	// fill with valid input
+  await firstDiffInput.fill(valA);
+  await secondDiffInput.fill(valB);
+
+  await expect(diffStatusIndicator).toHaveClass(/diff-status-same/);
 
 });
