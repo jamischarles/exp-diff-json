b48d479dd6d1b3fc422e4628c21f4f356c2bdf84
Jamis Charles <jamis82@webutah.com>
2022-12-03 17:29:37 -0700

    chore: Refactored test to have more reusable pieces with POM pattern

--- tests/e2e/happy-path-core-features.spec.js
+++ tests/e2e/happy-path-core-features.spec.js
@@ -1,130 +1,108 @@
 // @ts-check
 import { test, expect } from "@playwright/test";
+import * as base from "@playwright/test";
+import { MainPage } from "./main-page.js";
 
-// SHARED logic
-// FIXME: Is there a more "playwright" way to do this?
-function navigateToFirstPageAndGetLocators() {}
+test.describe("UI on the page...", () => {
+  // test hook. Doesn't need the describe block to work
+  test.beforeEach(async ({ page }) => {
+    const mainPage = new MainPage(page);
+    await mainPage.goto();
+  });
 
-// FIXME: How can I group these better by feature? Different files?
+  // FIXME: Split 2nd and first half out
+  test("shows 2 input fields. If JSON string in fields MATCHES indicator shows GREEN", async ({
+    page,
+  }) => {
+    const mainPage = new MainPage(page);
+    await mainPage.goto();
 
-// main UI
-test("shows 2 input fields. If JSON string in fields MATCHES indicator shows GREEN", async ({
-  page,
-  baseURL,
-}) => {
-  await page.goto(baseURL + "/");
+    const valA = `{"a": "testA", "b": "testB"}`;
+    const valB = `{"a": "testA", "b": "testB"}`;
 
-  // am I on the right page?
-  await expect(page).toHaveTitle(/Differ/);
+    // fill with valid input
+    await mainPage.firstDiffInput.fill(valA);
+    await mainPage.secondDiffInput.fill(valB);
 
-  const valA = `{"a": "testA", "b": "testB"}`;
-  const valB = `{"a": "testA", "b": "testB"}`;
+    await expect(mainPage.diffStatusIndicator).toHaveClass(/diff-status-same/);
+  });
 
-  // locators
-  const firstDiffInput = page.getByTestId("diff-field-a");
-  const secondDiffInput = page.getByTestId("diff-field-b");
-  const diffStatusIndicator = page.getByTestId("diff-status-indicator");
+  test("shows 2 input fields. If JSON string in fields DOES NOT MATCH indicator shows RED", async ({
+    page,
+  }) => {
+    const mainPage = new MainPage(page);
+    await mainPage.goto();
 
-  // fill with valid input
-  await firstDiffInput.fill(valA);
-  await secondDiffInput.fill(valB);
+    const valA = `{"a": "testA"}`;
+    const valB = `{"b": "testB"}`;
 
-  await expect(diffStatusIndicator).toHaveClass(/diff-status-same/);
+    // fill with valid input
+    await mainPage.firstDiffInput.fill(valA);
+    await mainPage.secondDiffInput.fill(valB);
+
+    await expect(mainPage.diffStatusIndicator).toHaveClass(
+      /diff-status-not-same/
+    );
+  });
 });
 
-test("shows 2 input fields. If JSON string in fields DOES NOT MATCH indicator shows RED", async ({
-  page,
-  baseURL,
-}) => {
-  await page.goto(baseURL + "/");
+// FIXME: Find better feature grouping / naming here...
+test.describe("JSON test features...", () => {
+  test("ignore JSON KEY order: 2 input fields indicate MATCH if JSON keys are in wrong order, but are otherwise identical", async ({
+    page,
+    baseURL,
+  }) => {
+    // FIXME: Can I somehow expose this from before hook? Maybe that's where the fixture comes in
+    const mainPage = new MainPage(page);
+    await mainPage.goto();
+    const { firstDiffInput, secondDiffInput, diffStatusIndicator } = mainPage;
 
-  // am I on the right page?
-  await expect(page).toHaveTitle(/Differ/);
+    const valA = `{"a": "testA", "b": "testB"}`;
+    const valB = `{"b": "testB", "a": "testA"}`;
 
-  const valA = `{"a": "testA"}`;
-  const valB = `{"b": "testB"}`;
+    // fill with valid input
+    await firstDiffInput.fill(valA);
+    await secondDiffInput.fill(valB);
 
-  // locators
-  const firstDiffInput = page.getByTestId("diff-field-a");
-  const secondDiffInput = page.getByTestId("diff-field-b");
-  const diffStatusIndicator = page.getByTestId("diff-status-indicator");
+    await expect(diffStatusIndicator).toHaveClass(/diff-status-same/);
+  });
 
-  // fill with valid input
-  await firstDiffInput.fill(valA);
-  await secondDiffInput.fill(valB);
+  // JSON valid / linting features
+  test("If json is malformed (parse error) in either window, indicate with red/green if it's valid / invalid syntax", async ({
+    page,
+    baseURL,
+  }) => {
+    const mainPage = new MainPage(page);
+    await mainPage.goto();
+    const { firstDiffInput, secondDiffInput, firstDiffStatusBar, secondDiffStatusBar, diffStatusIndicator } = mainPage;
 
-  await expect(diffStatusIndicator).toHaveClass(/diff-status-not-same/);
-});
+    const valAInvalid = `a": "testA", "b": "testB"}`;
+    const valBInvalid = `{"b": testB", "a" "testA"`;
 
-// JSON DIFF FEATURES
-test("ignore JSON KEY order: 2 input fields indicate MATCH if JSON keys are in wrong order, but are otherwise identical", async ({
-  page,
-  baseURL,
-}) => {
-  await page.goto(baseURL + "/");
+    const valAValid = `{"a": "testA", "b": "testB"}`;
+    const valBValid = `{"b": "testB", "a": "testA"}`;
 
-  // am I on the right page?
-  await expect(page).toHaveTitle(/Differ/);
+    // fill both with IN-VALID input
+    await firstDiffInput.fill(valAInvalid);
+    await secondDiffInput.fill(valBInvalid);
 
-  const valA = `{"a": "testA", "b": "testB"}`;
-  const valB = `{"b": "testB", "a": "testA"}`;
+    await expect(firstDiffStatusBar).toHaveText("INVALID JSON");
+    await expect(secondDiffStatusBar).toHaveText("INVALID JSON");
 
-  // locators
-  const firstDiffInput = page.getByTestId("diff-field-a");
-  const secondDiffInput = page.getByTestId("diff-field-b");
-  const diffStatusIndicator = page.getByTestId("diff-status-indicator");
+    // fill both with VALID input
+    await firstDiffInput.fill(valAValid);
+    await secondDiffInput.fill(valBValid);
 
-  // fill with valid input
-  await firstDiffInput.fill(valA);
-  await secondDiffInput.fill(valB);
+    await expect(firstDiffStatusBar).toHaveText("Valid JSON");
+    await expect(secondDiffStatusBar).toHaveText("Valid JSON");
 
-  await expect(diffStatusIndicator).toHaveClass(/diff-status-same/);
-});
+    // one Valid, one invalid
+    await firstDiffInput.fill(valAValid);
+    await secondDiffInput.fill(valBInvalid);
 
-// JSON valid / linting features
-test("If json is malformed (parse error) in either window, indicate with red/green if it's valid / invalid syntax", async ({
-  page,
-  baseURL,
-}) => {
-  await page.goto(baseURL + "/");
-
-  // am I on the right page?
-  await expect(page).toHaveTitle(/Differ/);
-
-  const valAInvalid = `a": "testA", "b": "testB"}`;
-  const valBInvalid = `{"b": testB", "a" "testA"`;
-
-  const valAValid = `{"a": "testA", "b": "testB"}`;
-  const valBValid = `{"b": "testB", "a": "testA"}`;
-
-  // locators
-  const firstDiffInput = page.getByTestId("diff-field-a");
-  const secondDiffInput = page.getByTestId("diff-field-b");
-  const diffStatusIndicator = page.getByTestId("diff-status-indicator");
-
-  const diffWindowAStatusBar = page.getByTestId("status-bar-a");
-  const diffWindowBStatusBar = page.getByTestId("status-bar-b");
-
-  // fill both with IN-VALID input
-  await firstDiffInput.fill(valAInvalid);
-  await secondDiffInput.fill(valBInvalid);
-
-  await expect(diffWindowBStatusBar).toHaveText("INVALID JSON");
-  await expect(diffWindowBStatusBar).toHaveText("INVALID JSON");
-
-  // fill both with VALID input
-  await firstDiffInput.fill(valAValid);
-  await secondDiffInput.fill(valBValid);
-
-  await expect(diffWindowAStatusBar).toHaveText("Valid JSON");
-  await expect(diffWindowBStatusBar).toHaveText("Valid JSON");
-
-  // one Valid, one invalid
-  await firstDiffInput.fill(valAValid);
-  await secondDiffInput.fill(valBInvalid);
-
-  await expect(diffWindowAStatusBar).toHaveText("Valid JSON");
-  await expect(diffWindowBStatusBar).toHaveText("INVALID JSON");
+    await expect(firstDiffStatusBar).toHaveText("Valid JSON");
+    await expect(secondDiffStatusBar).toHaveText("INVALID JSON");
+  });
 });
 
 // if either side (or both) has invalid JSON UI should correctly show if both sides match or not
---
+++ tests/e2e/main-page.js
@@ -0,0 +1,47 @@
+// Page object model of the main page so we can reuse locators and page for common actions
+// https://playwright.dev/docs/pom
+
+import { expect } from "@playwright/test";
+
+
+// test hooks are good for resuable pieces that don't export anything (like navigation & setup / teardown)
+// fixtures are great for things that need to return stuff (page, baseUrl are fixtures )
+//
+// use https://playwright.dev/docs/pom for resuable parts we want to expose
+// use fixtures for when you want to call parts of the POM (like "AddTodoItem") etc...
+//https://playwright.dev/docs/pom
+
+
+// page object model
+export class MainPage {
+  /**
+   * @param {import('@playwright/test').Page} page
+   */
+  constructor(page) {
+    this.page = page;
+
+    this.firstDiffInput = page.getByTestId("diff-field-a");
+    this.secondDiffInput = page.getByTestId("diff-field-b");
+
+    this.firstDiffStatusBar = page.getByTestId("status-bar-a");
+    this.secondDiffStatusBar = page.getByTestId("status-bar-b");
+
+    this.diffStatusIndicator = page.getByTestId("diff-status-indicator");
+  }
+
+  async goto() {
+    await this.page.goto("/");
+    await expect(this.page).toHaveTitle(/Differ/);
+  }
+
+  //  async getStarted() {
+  //    await this.getStartedLink.first().click();
+  //    await expect(this.gettingStartedHeader).toBeVisible();
+  //  }
+  //
+  // // Q: What does this do?
+  //  async pageObjectModel() {
+  //    await this.getStarted();
+  //    await this.pomLink.click();
+  //  }
+}
